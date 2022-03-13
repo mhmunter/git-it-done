82e1d73390d6e13e090ccbb6e5ddfa7145b8732f
M Munter <mark.munter1@gmail.com>
2022-03-13 14:53:12 -0500

    added last code

--- single.js
+++ single.js
@@ -1,5 +1,24 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
 
 var getRepoIssues = function (repo) {
     console.log(repo);
@@ -22,7 +41,9 @@ var getRepoIssues = function (repo) {
         }
     });
 }
-getRepoIssues("facebook/react");
+getRepoIssues();
+
+
 
 
 
@@ -76,3 +97,9 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
   };
+
+
+
+
+
+  getRepoName()
